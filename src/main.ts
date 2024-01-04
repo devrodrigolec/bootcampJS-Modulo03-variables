52aa84938c54ca5ae51b5f94f2e77d6fccd4758a
Rodrigo <devrodrigolec@gmail.com>
2024-01-04 20:02:49 +0100

    agregado compositor a interface

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,38 @@
-interface Banda {
-  nombre: string;
+interface GrupoMusical {
+  nombreGrupoMusical: string | null;
+  compositor: string | null
   anio: number;
   activo: boolean;
 }
 
-const theBeatles: Banda = {
-  nombre: "The Beatles",
+const theBeatles: GrupoMusical = {
+  nombreGrupoMusical: "The Beatles",
+  compositor: null,
   anio: 1960,
   activo: false,
 };
-const acdc: Banda = {
-  nombre: "ACDC",
+const acdc: GrupoMusical = {
+  nombreGrupoMusical: "ACDC",
+  compositor: null,
   anio: 1973,
   activo: true,
 };
-const queen: Banda = {
-  nombre: "Queen",
+const queen: GrupoMusical = {
+  nombreGrupoMusical: "Queen",
+  compositor: null,
   anio: 1970,
   activo: false,
 };
-const theRollingStones: Banda = {
-  nombre: "The Rolling Stones",
+const theRollingStones: GrupoMusical = {
+  nombreGrupoMusical: "The Rolling Stones",
+  compositor: null,
   anio: 1962,
   activo: true,
 };
 
-const Beethoven: Banda = {
-  nombre: "Ludwig van Beethoven",
+const Beethoven: GrupoMusical = {
+  nombreGrupoMusical: null,
+  compositor: "Ludwig van Beethoven",
   anio: 1770,
   activo: false,
 };
@@ -41,35 +47,35 @@ const estiloNombreDeGrupo: string =
 
 /* The Beatles*/
 
-console.log(`%c${theBeatles.nombre}`, estiloNombreDeGrupo);
+console.log(`%c${theBeatles.nombreGrupoMusical}`, estiloNombreDeGrupo);
 console.log(`Año: ${theBeatles.anio}`);
 console.log(`Activo: ${theBeatles.activo}`);
 console.log(`Género: ${popRock}`);
 
 /* Queen */
 
-console.log(`%c${queen.nombre}`, estiloNombreDeGrupo);
+console.log(`%c${queen.nombreGrupoMusical}`, estiloNombreDeGrupo);
 console.log(`Año: ${queen.anio}`);
 console.log(`Activo: ${queen.activo}`);
 console.log(`Género: ${rock}`);
 
 /* AC DC */
 
-console.log(`%c${acdc.nombre}`, estiloNombreDeGrupo);
+console.log(`%c${acdc.nombreGrupoMusical}`, estiloNombreDeGrupo);
 console.log(`Año: ${acdc.anio}`);
 console.log(`Activo: ${acdc.activo}`);
 console.log(`Género: ${hardRock}`);
 
 /* Beethoven */
 
-console.log(`%c${Beethoven.nombre}`, estiloNombreDeGrupo);
+console.log(`%c${Beethoven.compositor}`, estiloNombreDeGrupo);
 console.log(`Año: ${Beethoven.anio}`);
 console.log(`Activo: ${Beethoven.activo}`);
 console.log(`Género: ${clasica}`);
 
 /* The Rolling Stones */
 
-console.log(`%c${theRollingStones.nombre}`, estiloNombreDeGrupo);
+console.log(`%c${theRollingStones.nombreGrupoMusical}`, estiloNombreDeGrupo);
 console.log(`Año: ${theRollingStones.anio}`);
 console.log(`Activo: ${theRollingStones.activo}`);
 console.log(`Género: ${rock}`);
